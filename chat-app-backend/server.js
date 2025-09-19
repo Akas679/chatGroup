e15f2e65600edeb7fa86e73c8863087a0ebe7f38
akas679 <akash9511081008@gmail.com>
2025-09-19 15:48:34 +0530

    first commit

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -25,7 +25,6 @@ app.get("/messages", async (req, res) => {
 
 // Serve React build files
 app.use(express.static(path.join(__dirname, "build")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
